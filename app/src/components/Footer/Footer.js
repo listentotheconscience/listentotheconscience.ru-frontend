4ae75d7fd169cc68bf72be43c356f2205773bb16
listentotheconscience <iseethatshitlikethesharingan@gmail.com>
2022-01-04 22:55:00 +0300

    v0.1

---
+++ app/src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+
+const Footer = (props) => {
+    const github = require('../../images/GitHub-Mark.eps');
+    return (
+        <div className="container-footer">
+            <div className="social-media">
+                <a href="https://github.com/listentotheconscience">
+                    <FontAwesomeIcon className="github" icon={faGithub} />
+                </a>
+                <p className="copy">listentotheconscience <b style={{color: 'magenta'}}>&copy; 2022</b></p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
